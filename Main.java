2adf0af3a66372fe3e607440e8ee648f0d2f50a7
ismetakpinar <104082595+ismetakpinar@users.noreply.github.com>
2022-10-26 19:25:51 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        double a ,b;
+        double c;
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Kısadik kenarı  Giriniz :");
+        a = inp.nextInt();
+
+        Scanner mnp = new Scanner(System.in);
+        System.out.print("Uzun dik kenarı  Giriniz :");
+        b  = mnp.nextInt();
+
+        c = Math.sqrt((a*a) + (b*b));
+
+        System.out.println("Üçgenin Alanı :"+ (a+b)/2);
+        System.out.println("Hipotenüs :" + c);
+
+
+    }
 }
